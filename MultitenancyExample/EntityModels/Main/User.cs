67e2237c887561b906a1d55fdfc22dfe3e2da0d3
Andreas S. Hjelle <andreas@barn-nett.no>
2025-01-22 17:08:51 +0100

    Initialize User Id with a new GUID by default

--- MultitenancyExample/EntityModels/Main/User.cs
+++ MultitenancyExample/EntityModels/Main/User.cs
@@ -2,7 +2,7 @@ namespace MultitenancyExample.EntityModels.Main
 {
     public class User
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Username { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
